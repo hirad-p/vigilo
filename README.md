1c920dfafd24441db3c0c21298173ac9618473b6
Hirad <hirad.pourtahmasbi@jitterbit.com>
2018-04-27 20:30:06 -0700

    Vigilo core code

---
+++ README.md
@@ -0,0 +1,2 @@
+# vigilo
+This repo contains my efforts to make a smart security system
---
+++ src/vigilo.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+from twilio.rest import Client
+
+# Pi and pin setup
+GPIO.setmode(GPIO.BCM) 
+DOOR_SENSOR_PIN = 18
+GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# Twilio setup
+account = 'secret'
+token = 'secret'
+txt_to = 'secret'
+txt_from = 'secret'
+twilio = Client(account, token)
+
+def send_text():
+    text = 'Intruder detected'
+    message = twilio.messages.create(to=txt_to, from_=txt_from, body=text)
+
+isOpen = 0
+oldIsOpen = None
+
+while True: 
+    oldIsOpen = isOpen 
+    isOpen = GPIO.input(DOOR_SENSOR_PIN)
+    print(isOpen)
+    
+    if (isOpen and (isOpen > oldIsOpen)):
+        send_text()
+        print("Door opened!")
+        
+    time.sleep(0.1)
+    
